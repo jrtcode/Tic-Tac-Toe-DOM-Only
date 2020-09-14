41243a5eac92feaede9bc040816646e61ebceafa
Jeffrey T <jrtcode9@gmail.com>
2020-09-14 14:23:01 -0700

    first commit

---
+++ TicTacToe/game.js
@@ -0,0 +1,118 @@
+var spot1 = document.querySelector('#one')
+var spot2 = document.querySelector('#two')
+var spot3 = document.querySelector('#three')
+var spot4 = document.querySelector('#four')
+var spot5 = document.querySelector('#five')
+var spot6 = document.querySelector('#six')
+var spot7 = document.querySelector('#seven')
+var spot8 = document.querySelector('#eight')
+var spot9 = document.querySelector('#nine')
+
+
+spot1.addEventListener('click',function(){
+  if (spot1.textContent === 'X'){
+    spot1.textContent = 'O'
+  }else if (spot1.textContent === 'O'){
+    spot1.textContent = '/'
+    spot1.style.color = 'white'
+  }else {
+    spot1.textContent = 'X'
+    spot1.style.color = 'black'
+  }
+})
+
+spot2.addEventListener('click',function(){
+  if (spot2.textContent === 'X'){
+    spot2.textContent = 'O'
+  }else if (spot2.textContent === 'O'){
+    spot2.textContent = '/'
+    spot2.style.color = 'white'
+  }else {
+    spot2.textContent = 'X'
+    spot2.style.color = 'black'
+  }
+})
+
+spot3.addEventListener('click',function(){
+  if (spot3.textContent === 'X'){
+    spot3.textContent = 'O'
+  }else if (spot3.textContent === 'O'){
+    spot3.textContent = '/'
+    spot3.style.color = 'white'
+  }else {
+    spot3.textContent = 'X'
+    spot3.style.color = 'black'
+  }
+})
+
+spot4.addEventListener('click',function(){
+  if (spot4.textContent === 'X'){
+    spot4.textContent = 'O'
+  }else if (spot4.textContent === 'O'){
+    spot4.textContent = '/'
+    spot4.style.color = 'white'
+  }else {
+    spot4.textContent = 'X'
+    spot4.style.color = 'black'
+  }
+})
+
+spot5.addEventListener('click',function(){
+  if (spot5.textContent === 'X'){
+    spot5.textContent = 'O'
+  }else if (spot5.textContent === 'O'){
+    spot5.textContent = '/'
+    spot5.style.color = 'white'
+  }else {
+    spot5.textContent = 'X'
+    spot5.style.color = 'black'
+  }
+})
+
+spot6.addEventListener('click',function(){
+  if (spot6.textContent === 'X'){
+    spot6.textContent = 'O'
+  }else if (spot6.textContent === 'O'){
+    spot6.textContent = '/'
+    spot6.style.color = 'white'
+  }else {
+    spot6.textContent = 'X'
+    spot6.style.color = 'black'
+  }
+})
+
+spot7.addEventListener('click',function(){
+  if (spot7.textContent === 'X'){
+    spot7.textContent = 'O'
+  }else if (spot7.textContent === 'O'){
+    spot7.textContent = '/'
+    spot7.style.color = 'white'
+  }else {
+    spot7.textContent = 'X'
+    spot7.style.color = 'black'
+  }
+})
+
+spot8.addEventListener('click',function(){
+  if (spot8.textContent === 'X'){
+    spot8.textContent = 'O'
+  }else if (spot8.textContent === 'O'){
+    spot8.textContent = '/'
+    spot8.style.color = 'white'
+  }else {
+    spot8.textContent = 'X'
+    spot8.style.color = 'black'
+  }
+})
+
+spot9.addEventListener('click',function(){
+  if (spot9.textContent === 'X'){
+    spot9.textContent = 'O'
+  }else if (spot9.textContent === 'O'){
+    spot9.textContent = '/'
+    spot9.style.color = 'white'
+  }else {
+    spot9.textContent = 'X'
+    spot9.style.color = 'black'
+  }
+})
